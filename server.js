71cc6798c8ffbcab793564af4902f76c3a4b1ebd
skanda1395 <skanda1395@gmail.com>
2021-05-21 00:45:15 +0530

    replaced sendfile with latest version

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
+const serveStatic = require("serve-static");
+const path = require("path");
+
 const port = process.env.PORT || 8080;
 
 // Handle static files
-app.use(express.static(__dirname + "/dist/"));
+app.use(serveStatic(path.join(__dirname, "/dist/")));
 
 // Serve index.html for every request
 app.get(/.*/, (req, res) => {
-  res.sendFile(__dirname, "/dist/index.html");
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
 // Spin up our server
